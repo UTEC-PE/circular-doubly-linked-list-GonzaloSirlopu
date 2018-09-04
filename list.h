e19428c1bf1b222238a9398ef08d40d5ef7808f7
GonzaloSirlopu <42593673+GonzaloSirlopu@users.noreply.github.com>
2018-09-04 18:49:56 -0500

    adicion

--- list.h
+++ list.h
@@ -13,23 +13,58 @@ class List {
         Node<T>* start;
 
     public:
-        List();
+        List(){
+            start=nullptr;
+            nodes=0;
+        };
 
-        T front();
-        T back();
-        void push_front(T value);
-        void push_back(T value);
-        void pop_front();
+        T front(){return start -> data;};
+        T back(){return start -> prev -> data;};
+        void push_front(T value){
+            Node<T>*temporal=new Node<T>;
+            temporal -> data = value;
+            if(start = nullptr){
+                temporal -> next = temporal -> prev = temporal;
+                start = temporal;
+            }
+            else{
+                Node<T>* last = start -> prev;
+                temporal -> next = start;
+                temporal -> prev = last;
+                last -> next = start -> prev;
+                start = temporal;
+
+            }
+        };
+        void push_back(T value){
+            Node<T>*temporal=new Node<T>;
+            temporal -> data = value;
+            if(start = nullptr){
+                temporal -> next = temporal -> prev = temporal;
+                start = temporal;
+            }
+            else{
+                Node<T>* last = start -> prev;
+                temporal -> next = start;
+                start -> prev = temporal;
+                temporal -> prev = last;
+                last -> next = temporal;
+            }
+        };
+        void pop_front(){
+            Node<T>*temporal=start;
+            start -> next = start-> next -> next;
+        };
         void pop_back();
         T get(int position);
         void concat(List<T> &other);
-        bool empty();
-        int size();
-        void clear();
-        Iterator<T> begin();
-        Iterator<T> end();
+        bool empty(){return nodes==0;};
+        int size(){return nodes;};
+        void clear(){};
+        Iterator<T> begin(){return Iterator<T>(start);};
+        Iterator<T> end(){return Iterator<T>(start->prev);};
 
-        ~List();
+        ~List(){};
 };
 
 #endif
